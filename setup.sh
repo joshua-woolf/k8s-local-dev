47012e4033743d89dbe8caa88c4487dfff2d0bdf
Joshua Woolf <joshuawoolf87@gmail.com>
2025-01-14 17:00:51 +0200

    add traefik

--- setup.sh
+++ setup.sh
@@ -3,3 +3,15 @@
 docker run -d --restart=always -p 5000:5000 --name registry --network kind registry:2
 
 kind create cluster --config kind-config.yaml
+
+## Traefik
+
+helm repo add traefik https://traefik.github.io/charts
+helm repo update
+
+helm upgrade traefik traefik/traefik \
+  --create-namespace \
+  --install \
+  --namespace traefik \
+  --values "./values/traefik-values.yaml" \
+  --wait
