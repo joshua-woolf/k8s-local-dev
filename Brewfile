3f2b59f4c552aa379d5ce3eaa4510e6cf4e69881
Joshua Woolf <joshuawoolf87@gmail.com>
2025-02-20 19:44:21 +0200

    add fluxcd setup

--- Brewfile
+++ Brewfile
@@ -1,3 +1,6 @@
+tap "fluxcd/tap"
+
+brew "fluxcd/tap/flux", version: "2.5.0"
 brew "git"
 brew "helm"
 brew "k9s"
@@ -5,5 +8,6 @@ brew "kind"
 brew "kubernetes-cli"
 brew "kustomize"
 brew "node"
+
 cask "cursor"
 cask "docker"
--- setup.sh
+++ setup.sh
@@ -102,6 +102,9 @@ declare -a IMAGES=(
   "docker.io/traefik:v2.11.2"
   "ghcr.io/fluxcd/flagger:1.40.0"
   "ghcr.io/fluxcd/flagger-loadtester:0.34.0"
+  "ghcr.io/fluxcd/helm-controller:v1.2.0"
+  "ghcr.io/fluxcd/kustomize-controller:v1.5.0"
+  "ghcr.io/fluxcd/source-controller:v1.5.0"
   "ghcr.io/kedacore/keda:2.16.1"
   "ghcr.io/kedacore/keda-metrics-apiserver:2.16.1"
   "ghcr.io/kedacore/keda-admission-webhooks:2.16.1"
@@ -157,6 +160,16 @@ for node in $(kind get nodes); do
     update-ca-certificates
   "
 done
+
+# Boostrap FluxCD
+flux bootstrap github \
+  --components source-controller,kustomize-controller,helm-controller \
+  --token-auth \
+  --owner=joshua-woolf \
+  --repository=k8s-lab \
+  --branch=main \
+  --registry=registry.local.dev:5001/ghcr.io/fluxcd \
+  --path=clusters/local-dev
 
 # Install Grafana dashboards
 helm upgrade grafana-dashboards ./charts/grafana-dashboards \
